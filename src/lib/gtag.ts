bc7c6c2aad4281a43c70996846c2c4d7231e3170
boobo94 <militaru.bogdanalexandru@gmail.com>
2026-07-29 11:55:01 +0300

    Update gtrack

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -7,5 +7,10 @@ declare global {
 
 export function trackEvent(eventName: string, params?: Record<string, unknown>) {
   if (typeof window === "undefined" || typeof window.gtag !== "function") return;
-  window.gtag("event", eventName, params);
+  window.gtag("event", eventName, {
+    page_location: window.location.href,
+    page_path: window.location.pathname + window.location.search,
+    page_title: document.title,
+    ...params,
+  });
 }
